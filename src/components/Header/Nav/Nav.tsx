2ca99d621faaa3da7cdeedc66972ccd436d25a49
bogdanatamanuk <0997393654b@gmail.com>
2022-12-17 21:14:32 +0100

    finish html and css refactor#2

--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -1,13 +1,17 @@
-import style from '../Header.module.css';
+import style from './Nav.module.css';
 import React from 'react';
+import styleContainer from './../../../common/styles/Container.module.css'
 
 export const Nav = () => {
     return (
-        <div className={style.nav}>
-            <a href="src/components/Header/Nav/Nav#">Home</a>
-            <a href="src/components/Header/Nav/Nav#">Skills</a>
-            <a href="src/components/Header/Nav/Nav#">Works</a>
-            <a href="src/components/Header/Nav/Nav#">Contacts</a>
-        </div>
+
+            <div className={style.nav}>
+                <a href="src/components/Header/Nav/Nav#">Home</a>
+                <a href="src/components/Header/Nav/Nav#">Skills</a>
+                <a href="src/components/Header/Nav/Nav#">Works</a>
+                <a href="src/components/Header/Nav/Nav#">Contacts</a>
+            </div>
+
+
     );
 };
